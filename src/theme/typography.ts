176f87dee4c4989a715076e1eb005cf8496e14ca
CarlosX26 <cjunior1@live.com>
2024-01-05 00:44:14 -0300

    feat: text props

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,7 +1,11 @@
 import styled from "styled-components/native"
 
-export const TextRegular = styled.Text`
-  font-size: 24px;
+interface ITextRegularProps {
+  fontSize?: number
+}
+
+export const TextRegular = styled.Text<ITextRegularProps>`
+  font-size: ${({ fontSize }) => (fontSize ? `${fontSize}px` : "24px")};
   color: ${({ theme }) => theme.COLORS.TEXT_COLOR};
   font-family: ${({ theme }) => theme.FONT.FAMILY};
 `

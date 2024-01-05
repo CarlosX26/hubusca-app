74748a541368a841d1dea9fa3a6c6bd2f9753234
CarlosX26 <cjunior1@live.com>
2024-01-05 00:43:10 -0300

    feat: icon history

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,7 +1,9 @@
+import { History } from "./History"
 import { Home } from "./Home"
 import { Search } from "./Search"
 
 export const Icons = {
   Search: Search,
   Home: Home,
+  History: History,
 }

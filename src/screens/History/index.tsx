2afb743c0760cf8896dc25e87f17f0524724fce2
CarlosX26 <cjunior1@live.com>
2024-01-08 13:25:09 -0300

    feat: open card user details

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -7,32 +7,47 @@ import {
   CardSearchedUser,
   HistoryWrapper,
 } from "./styles"
+import { useState } from "react"
+import { CardUserDetails } from "../../components/CardUserDetails"
 
 export const History = () => {
-  const { profileHistory } = useGithubContext()
+  const [showCard, setShowCard] = useState(false)
+  const { profileHistory, toggleCurrentUser } = useGithubContext()
+
+  const closeCard = () => {
+    setShowCard(false)
+  }
 
   return (
-    <HistoryWrapper>
-      <TextRegular fontSize={32}>Buscas recentes</TextRegular>
+    <>
+      <HistoryWrapper>
+        <TextRegular fontSize={32}>Buscas recentes</TextRegular>
 
-      <FlatList
-        style={{ marginTop: 32 }}
-        data={profileHistory}
-        renderItem={({ item }) => (
-          <CardSearchedUser>
-            <CardSearchedAvatar source={{ uri: item.avatar_url }} />
+        <FlatList
+          style={{ marginTop: 32 }}
+          data={profileHistory}
+          renderItem={({ item }) => (
+            <CardSearchedUser
+              onPress={() => {
+                setShowCard(true)
+                toggleCurrentUser(item)
+              }}
+            >
+              <CardSearchedAvatar source={{ uri: item.avatar_url }} />
 
-            <BoxInfo>
-              <TextRegular fontSize={20}>{item.name}</TextRegular>
-              <TextRegular fontSize={20}>{item.login}</TextRegular>
-              {item.location && (
-                <TextRegular fontSize={20}>{item.location}</TextRegular>
-              )}
-            </BoxInfo>
-          </CardSearchedUser>
-        )}
-        keyExtractor={(item) => item.login}
-      />
-    </HistoryWrapper>
+              <BoxInfo>
+                <TextRegular fontSize={20}>{item.name}</TextRegular>
+                <TextRegular fontSize={20}>{item.login}</TextRegular>
+                {item.location && (
+                  <TextRegular fontSize={20}>{item.location}</TextRegular>
+                )}
+              </BoxInfo>
+            </CardSearchedUser>
+          )}
+          keyExtractor={(item) => item.login}
+        />
+      </HistoryWrapper>
+      {showCard && <CardUserDetails closeCard={closeCard} />}
+    </>
   )
 }
